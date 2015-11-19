63f5f43ebcf6b44abf6b3411e50ae9abc078509d
Alexander Ivanenko <alexander.ivanenko@gmail.com>
2015-11-19 20:18:06 +0100

    more work on gui

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ time_to_stop = threading.Event()
 
 tracking_thread = threading.Thread(target = do_tracking, args = (frames, time_to_stop))
 
-tracking_thread.start()
+# tracking_thread.start()
 
 root = Tk.Tk()
 
@@ -28,23 +28,69 @@ def quit():
     time_to_stop.set()    
     tracking_thread.join()
     root.quit()
+    
+def set_image(container, matrix):
+    img = Image.fromarray(matrix)
+    imgtk = ImageTk.PhotoImage(image = img) 
+    container.configure(image = imgtk)
+    container.image = imgtk        
 
+cap = cv2.VideoCapture('videotest.avi')
+cap.set(cv2.CAP_PROP_POS_FRAMES, 190)
+
+image_container = Tk.Label(root)
+image_container.pack()
+
+max_video_position_slider_value = 100
+
+def on_video_position_changed(val):
+    max = cap.get(cv2.CAP_PROP_FRAME_COUNT);
+    cap.set(cv2.CAP_PROP_POS_FRAMES, max * float(val) / max_video_position_slider_value)
+    ret, frame = cap.read()
+    set_image(image_container, frame)
+            
+    
 button = Tk.Button(root, text = "Quit", command = quit)
 button.pack(side = Tk.LEFT)
 
-imglabel = Tk.Label(root)
-imglabel.pack()
+slider = Tk.Scale(root, length = 300, from_ = 0, to = max_video_position_slider_value, 
+                  orient = Tk.HORIZONTAL, command = on_video_position_changed)
+                  
+slider.pack(side = Tk.BOTTOM)
+
+start_x = 0
+start_y = 0
+end_x = 0
+end_y = 0
+
+def on_left_mouse_button_down(event):
+    start_x = event.x
+    start_y = event.y
+
+def on_left_mouse_button_up(event):
+    end_x = event.x
+    end_y = event.y
+    
+def on_mouse_moved(event):
+    return 0
+
+image_container.bind('<Button-1>', on_left_mouse_button_down)
+image_container.bind('<ButtonRelease-1>', on_left_mouse_button_up)
+image_container.bind('<Motion>', on_mouse_moved)
+
+# take first frame of the video
+ret, frame = cap.read()
+if not ret:
+    print('can\'t read the video')
+    sys.exit()
+
+set_image(image_container, frame)
 
 def poll_queue():
-    
     if not frames.empty():
         frame = frames.get()
-
-        img = Image.fromarray(frame)
-        imgtk = ImageTk.PhotoImage(image = img) 
-   
-        imglabel.configure(image = imgtk)
-        imglabel.image = imgtk        
+        set_image(image_container, frame)
+     
     
     root.after(100, poll_queue)
 
